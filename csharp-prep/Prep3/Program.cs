93e505bbef773d2fd96c828bdadd5aa86b668edc
Jonathan Murillo <jonathan.murillo5678@gmail.com>
2023-09-16 17:36:17 -0600

    First Attempt: Have specified number to guess and display message

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Console.Write("What is the magic number? ");
+        string magicnum = Console.ReadLine();
+        Console.Write("What is your guess? ");
+        string guessnum = Console.ReadLine();
+
+        int magic = int.Parse(magicnum);
+        int guess = int.Parse(guessnum);
+
+        while (guess < magic)
+        {
+            Console.WriteLine("Higher");
+            break;
+        }
+        while (guess > magic)
+        {
+            Console.WriteLine("Lower");
+            break;
+        }
+        while (guess == magic)
+        {
+            Console.WriteLine("You guess correctly!");
+            break;
+        }
+
     }
 }
