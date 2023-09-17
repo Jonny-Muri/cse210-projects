2d36fab85899298998cc4d7949990d8f5f480e1c
Jonathan Murillo <jonathan.murillo5678@gmail.com>
2023-09-16 20:40:19 -0600

    Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,28 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void DisplayWelcom()
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static void PromptUsernName(string username)
+    {
+        Console.WriteLine($"Hello {username}");
+    }
+
+    static void PromptUserNumber()
+    {
+        Console.WriteLine$""
+    }
+
+    static void SquareNumber()
+    {
+
+    }
+
+    static void DisplayResult()
+    {
+
     }
 }
