3a7658a69238222cb7d0549febf03697e0ea993b
Jonathan Murillo <jonathan.murillo5678@gmail.com>
2023-09-23 22:18:36 -0600

    Initial commit

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,15 +6,7 @@ public class PromptGenerator
 
     public void GetRandomPrompt()
     {
-        _prompts.Add("What was the best part of my day?");
-        _prompts.Add("How did I see the hand of the Lord in my life today?");
-        _prompts.Add("If I had one thing I could do over today, what would it be?");
-        _prompts.Add("What am I nervous or anxious about today?");
-        _prompts.Add("What were the highlights of my day");
-
-        Random random = new Random();
-        random.Next(_prompts.Count);
-        Console.WriteLine(_prompts);
+        Console.WriteLine("Hellow Sandboxworld");
 
     }
 }
