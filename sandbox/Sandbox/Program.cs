2d3738846caa738c29a16059c6947915ea048fec
Jonathan Murillo <jonathan.murillo5678@gmail.com>
2023-09-30 19:57:37 -0600

    Intial Commit

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,34 +1,25 @@
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
-
     static void Main(string[] args)
-
     {
-        string[] wordsToHide = { "apple", "banana", "cherry", "date", "grape", "kiwi", "mango", "orange", "pear", "strawberry" };
-
-        while (true)
-        {
-            Console.Clear();
-            string sentence = "I like to eat apple, banana, cherry, and strawberry.";
-
-            // Randomly select words to hide
-            Random random = new Random();
-            int numberOfWordsToHide = random.Next(1, 4); // Randomly choose 1 to 3 words to hide
-
-            for (int i = 0; i < numberOfWordsToHide; i++)
-            {
-                int wordIndex = random.Next(wordsToHide.Length);
-                string wordToHide = wordsToHide[wordIndex];
-                sentence = sentence.Replace(wordToHide, new string('*', wordToHide.Length));
-            }
-
-            Console.WriteLine(sentence);
-            Thread.Sleep(2000); // Wait for 2 seconds before clearing the console again
-        }
+        Person p0 = new Person();
+        p0.setFirst("Jonathan");
+        Console.WriteLine(p0.getFirst());
+    }
+}
+public class Person
+{
+    private string _firstName;
+    public void setFirst(string firstname)
+    {
+        _firstName = firstname;
+    }
+    public string getFirst()
+    {
+        return _firstName;
     }
 
-
-    
 }
