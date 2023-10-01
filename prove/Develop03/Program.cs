daca86bf77d3b44c782d6ec003858f110175d881
Jonathan Murillo <jonathan.murillo5678@gmail.com>
2023-09-30 20:53:58 -0600

    Scripture Memorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,13 +5,13 @@ class Program
     static void Main(string[] args)
     {   
         Reference refer = new Reference("Proverbs", 3, 5, 6);
-        //string prov = "Trust in the Lord with all thine heart; and lean not unto thine own understanding";
         Scripture script = new Scripture(refer, "Trust in the Lord with all thine heart; and lean not unto thine own understanding");
         
+        Console.Clear();
         
-        Console.WriteLine(refer.GetDisplayText());
-
         Console.WriteLine(script.GetDisplayText());
-
+        Console.WriteLine("Press enter to continue or type 'quit' to finish");
+        Console.ReadLine();
+        
     }
 }
