fa6c23c237229fead288414d32a05ff766327c90
Jonathan Murillo <jonathan.murillo5678@gmail.com>
2023-10-03 20:24:52 -0600

    Breathing

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        int input = -1;
+        while (input != 4)
+        {
+            Console.WriteLine("Menu Options");
+            Console.WriteLine(" 1. Start breathing activity");
+            Console.WriteLine(" 2. Start reflecting activity");
+            Console.WriteLine(" 3. Start listing activity");
+            Console.WriteLine(" 4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            string user = Console.ReadLine();
+            input = int.Parse(user);
+
+            
+
+            switch (input)
+            {
+                case 1:
+                break;
+
+                case 2:
+                break;
+
+                case 3:
+                break;
+
+                case 4:
+                break;
+            }
+
+
+
+        }
     }
 }
