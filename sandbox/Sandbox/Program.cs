56f9b90891163747f7679789d959cd7ddb35b17e
Jonathan Murillo <jonathan.murillo5678@gmail.com>
2023-10-07 18:48:48 -0600

    Sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,63 +5,27 @@ class Program
 {
     static void Main(string[] args)
     {
+        Student student = new Student();
+        string name = student.GetName();
+        string num = student.GetNumber();
+        Console.WriteLine(name);
+        Console.WriteLine(num);
+    }
 
-        //for (int i = 5; i > 0; i--)
-       // {
-       //     Console.Write(i);
-       //     Thread.Sleep(1000);
-       //     Console.Write("\b \b");
-       // }
-
-        Console.WriteLine("Loading...");
-        List<string> animationSymbol = new List<string>();
-        animationSymbol.Add("|");
-        animationSymbol.Add("/");
-        animationSymbol.Add("-");
-        animationSymbol.Add("\\");
-        animationSymbol.Add("|");
-        animationSymbol.Add("/");
-        animationSymbol.Add("-");
-        animationSymbol.Add("\\");
-
-        DateTime startTime = DateTime.Now;
-        DateTime endTime = startTime.AddSeconds(1);
-
-        int i = 0;
-
-        while(DateTime.Now < endTime)
+    public class Person
+    {
+        public string GetName()
         {
-            string s = animationSymbol[i];
-            Console.Write(s);
-            Thread.Sleep(100);
-            Console.Write("\b \b");
-
-            i++;
-
-            if (i >= animationSymbol.Count)
-            {
-                i = 0;
-            }
+            return "Johnny";
         }
-        Console.Clear();
-        Console.WriteLine("Loading Complete!");
-
-
-       // Console.Clear();
-       // Console.WriteLine("Loading... ");
-
-       // char[] animationChars = {'|', '/', '-', '\\'};
-
-       // for (int i = 0; i < 20; i++)
-       // {
-       //     char animationChar = animationChars[i % animationChars.Length];
-
-       //     Console.Write("\r" + animationChar);
-
-       //     Thread.Sleep(100);
-       // }
-       // Console.Clear();
-       // Console.WriteLine("Loading complete!");
 
     }
+    public class Student : Person
+    {
+        public string GetNumber()
+        {
+            return "058794622";
+        }
+    }
+
 }
