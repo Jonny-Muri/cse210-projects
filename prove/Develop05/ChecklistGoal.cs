ae5348ba82f61fabc5ca35f804ee886ce5a1a3fe
Jonathan Murillo <jonathan.murillo5678@gmail.com>
2023-10-11 20:12:47 -0600

    Goal Tracker

---
+++ prove/Develop05/ChecklistGoal.cs
@@ -0,0 +1,33 @@
+using System;
+
+public class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+    public ChecklistGoal(string name, string description, string points) : base(name, description, points)
+    {
+
+    }
+
+    public override void RecordEvent()
+    {
+
+    }
+
+    public override bool IsComplete()
+    {
+        return true;
+    }
+
+    public string GetDetailsString()
+    {
+        return "";
+    }
+     
+    public override string GetStringRepresentation()
+    {
+        return "";
+    }
+}
---
+++ prove/Develop05/GoalManager.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+
+public class GoalManager
+{
+    List<Goal> _goals;
+    int _score;
+    public GoalManager()
+    {
+
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void DisplayPlayerInfo()
+    {
+
+    }
+    //Case 2
+    public void ListGoalNames()
+    {
+
+    }
+    //Case 2
+    public void ListGoalDetails()
+    {
+
+    }
+    //Case 1
+    public void CreateGoal()
+    {
+
+    }
+    //Case 5
+    public void RecordEvent()
+    {
+
+    }
+    //Case 3
+    public void SaveGoals()
+    {
+
+    }
+    //Case 4
+    public void LoadGoals()
+    {
+
+    }
+}
