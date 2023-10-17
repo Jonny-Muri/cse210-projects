2ecc5ad1842925cd5a3bea6018485839c9bd2c24
Jonathan Murillo <jonathan.murillo5678@gmail.com>
2023-10-16 22:36:19 -0600

    Event Planning

---
+++ final/Foundation3/Event.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Event
+{
+    protected string _title;
+    protected string _description;
+    
+
+    public void standardDetail()
+    {
+        
+    }
+}
---
+++ final/Foundation3/Outdoor.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Outdoor
+{
+    public Outdoor()
+    {
+
+    }
+}
